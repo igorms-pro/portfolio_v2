10d7edeb50823e708e9b4ef756886b736662a46c
igorms-pro <igorms.pro@gmail.com>
2025-06-30 10:22:11 +0200

    showcase navbar added

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,20 +1,44 @@
+import { navLinks } from '@/constants.ts';
+import { useEffect, useState } from 'react';
+
 const NavBar = () => {
+  const [scrolled, setScrolled] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      const isScrolled: boolean = window.scrollY > 10;
+      setScrolled(isScrolled);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
   return (
-    <header className="navbar">
+    <header className={`navbar ${scrolled ? 'scrolled' : 'not-scrolled'}`}>
       <div className="inner">
         <a className="logo" href="#hero">
           © Igor MS
         </a>
         <nav className="desktop">
-          <ul></ul>
+          <ul>
+            {navLinks.map(({ link, name }, index) => (
+              <li key={`${name}-${index}`}>
+                <a href={link}>
+                  <span>{name}</span>
+                  <span className="underline" />
+                </a>
+              </li>
+            ))}
+          </ul>
         </nav>
         <a className="contact-btn group">
           <div className="inner">
-            <span> Contact me</span>
+            <span>Contact me</span>
           </div>
         </a>
       </div>
     </header>
   );
 };
+
 export default NavBar;
--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,9 @@
-export const words = [
+export type WordItem = {
+  text: string;
+  imgPath: string;
+};
+
+export const words: WordItem[] = [
   { text: 'Ideas', imgPath: '/images/ideas.svg' },
   { text: 'Concepts', imgPath: '/images/concepts.svg' },
   { text: 'Designs', imgPath: '/images/designs.svg' },
@@ -9,28 +14,27 @@ export const words = [
   { text: 'Code', imgPath: '/images/code.svg' },
 ];
 
-export const counterItems = [
+export type CounterItem = {
+  value: number;
+  suffix: string;
+  label: string;
+};
+
+export const counterItems: CounterItem[] = [
   { value: 9, suffix: '+', label: 'Years of Experience' },
   { value: 20, suffix: '+', label: 'Satisfied Clients' },
   { value: 108, suffix: '+', label: 'Completed Projects' },
   { value: 85, suffix: '%', label: 'Client Retention Rate' },
 ];
 
-export const navLinks = [
-  {
-    name: 'Work',
-    link: '#work',
-  },
-  {
-    name: 'Experience',
-    link: '#experience',
-  },
-  {
-    name: 'Skills',
-    link: '#skills',
-  },
-  {
-    name: 'Testimonials',
-    link: '#testimonials',
-  },
+export type NavLink = {
+  name: string;
+  link: string;
+};
+
+export const navLinks: NavLink[] = [
+  { name: 'Work', link: '#work' },
+  { name: 'Experience', link: '#experience' },
+  { name: 'Skills', link: '#skills' },
+  { name: 'Testimonials', link: '#testimonials' },
 ];
